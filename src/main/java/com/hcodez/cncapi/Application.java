89c27242caad62f78b790f21c4c5b004b0886ef5
cezarmathe <cezarmathe@gmail.com>
2019-08-06 18:07:25 +0300

    Configure app entry point

--- src/main/java/com/hcodez/cncapi/Application.java
+++ src/main/java/com/hcodez/cncapi/Application.java
@@ -4,7 +4,9 @@
 package com.hcodez.cncapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
