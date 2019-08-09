8c983e0921ace5aa742746025a98eaf50f9b8d2d
cezarmathe <cezarmathe@gmail.com>
2019-08-09 20:41:55 +0300

    Add method for getting CodeEntity by identifier and owner

--- src/main/java/com/hcodez/cncapi/code/CodeController.java
+++ src/main/java/com/hcodez/cncapi/code/CodeController.java
@@ -4,10 +4,7 @@ import com.hcodez.codeengine.model.Code;
 import com.hcodez.codeengine.model.CodeType;
 import com.hcodez.codeengine.parser.CodeParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +21,10 @@ public class CodeController {
         return new ArrayList<>();
     }
 
-    @RequestMapping(value = "/parse", method = RequestMethod.POST, consumes = "application/text", produces = "application/json")
+    @RequestMapping(value = "/parse",
+            method = RequestMethod.POST,
+            consumes = "application/text",
+            produces = "application/json")
     public List<String> parseInputString(@RequestBody String input) {
         final List<Code> codeList = new CodeParser()
                 .addCodeTypes(CodeType.all())
@@ -36,5 +36,11 @@ public class CodeController {
         }
 
         return jsonStringList;
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            consumes = "application/json")
+    public List<CodeEntity> getByIdentifierAndOwner(@RequestParam String identifier, @RequestParam String owner) {
+        return codeRepository.findByIdentifierAndOwner(identifier, owner);
     }
 }
--- src/main/java/com/hcodez/cncapi/code/CodeRepository.java
+++ src/main/java/com/hcodez/cncapi/code/CodeRepository.java
@@ -2,5 +2,8 @@ package com.hcodez.cncapi.code;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CodeRepository extends CrudRepository<CodeEntity, Integer> {
+    List<CodeEntity> findByIdentifierAndOwner(String identifier, String owner);
 }
