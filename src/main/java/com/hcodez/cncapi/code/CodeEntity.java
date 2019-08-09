4134fa8f4c801ac17f2bb9630fa118be43461fbb
cezarmathe <cezarmathe@gmail.com>
2019-08-09 14:06:28 +0300

    Add lombok annotations for CodeEntity

--- src/main/java/com/hcodez/cncapi/code/CodeEntity.java
+++ src/main/java/com/hcodez/cncapi/code/CodeEntity.java
@@ -3,105 +3,51 @@ package com.hcodez.cncapi.code;
 import com.hcodez.codeengine.builder.CodeBuilder;
 import com.hcodez.codeengine.model.Code;
 import com.hcodez.codeengine.model.CodeType;
+import lombok.*;
 import org.joda.time.Instant;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
 
 @Component
+@Data
+@NoArgsConstructor
+@Builder
 public class CodeEntity {
 
-    private int cloud_id;
+    private int cloudId;
 
+    @Getter
+    @Setter
     private String identifier;
 
+    @Getter
+    @Setter
     private String owner;
 
+    @Getter
+    @Setter
     private String passcode;
 
+    @Getter
+    @Setter
     private String name;
 
+    @Getter
+    @Setter
     private URL url;
 
+    @Getter
+    @Setter
     private Instant createTime;
 
+    @Getter
+    @Setter
     private Instant updateTime;
 
+    @Getter
+    @Setter
     private CodeType codeType;
-
-    public CodeEntity() {}
-
-    public int getCloud_id() {
-        return cloud_id;
-    }
-
-    public void setCloud_id(int cloud_id) {
-        this.cloud_id = cloud_id;
-    }
-
-    public String getIdentifier() {
-        return identifier;
-    }
-
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public String getPasscode() {
-        return passcode;
-    }
-
-    public void setPasscode(String passcode) {
-        this.passcode = passcode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public URL getUrl() {
-        return url;
-    }
-
-    public void setUrl(URL url) {
-        this.url = url;
-    }
-
-    public Instant getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Instant createTime) {
-        this.createTime = createTime;
-    }
-
-    public Instant getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Instant updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public CodeType getCodeType() {
-        return codeType;
-    }
-
-    public void setCodeType(CodeType codeType) {
-        this.codeType = codeType;
-    }
 
     /**
      * Create a library Code model from a CodeEntity
