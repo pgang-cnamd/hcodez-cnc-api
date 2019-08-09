fa387c58e87b81bfd6fbb65799e766a2ee7627ee
cezarmathe <cezarmathe@gmail.com>
2019-08-10 02:56:52 +0300

    Remove jackson

--- src/main/java/com/hcodez/cncapi/Application.java
+++ src/main/java/com/hcodez/cncapi/Application.java
@@ -5,8 +5,9 @@ package com.hcodez.cncapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
