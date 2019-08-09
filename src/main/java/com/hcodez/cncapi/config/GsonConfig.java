7a259ac6d7640cec619454dc19f03682dfb29239
cezarmathe <cezarmathe@gmail.com>
2019-08-10 02:57:21 +0300

    Fix gson config

--- src/main/java/com/hcodez/cncapi/config/GsonConfig.java
+++ src/main/java/com/hcodez/cncapi/config/GsonConfig.java
@@ -9,17 +9,33 @@ import com.hcodez.codeengine.json.serialization.InstantDeserializer;
 import com.hcodez.codeengine.json.serialization.InstantSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class ApplicationConfiguration {
+@EnableWebMvc
+public class GsonConfig implements WebMvcConfigurer {
 
-    @Bean
-    public Gson gson() {
-        return new GsonBuilder()
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(gsonHttpMessageConverter());
+    }
+
+    private GsonHttpMessageConverter gsonHttpMessageConverter() {
+        final Gson gson = new GsonBuilder()
                 .registerTypeAdapter(CodeTypeSerializer.class, new CodeTypeSerializer())
                 .registerTypeAdapter(CodeTypeDeserializer.class, new CodeTypeDeserializer())
                 .registerTypeAdapter(InstantSerializer.class, new InstantSerializer())
                 .registerTypeAdapter(InstantDeserializer.class, new InstantDeserializer())
                 .create();
+
+        final GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+        gsonHttpMessageConverter.setGson(gson);
+
+        return gsonHttpMessageConverter;
     }
 }
