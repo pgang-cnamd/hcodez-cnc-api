7515edec9322f7381d967fcf3394e529ab747cae
cezarmathe <cezarmathe@gmail.com>
2019-08-10 16:08:55 +0300

    Update CodeController

--- src/main/java/com/hcodez/cncapi/code/CodeController.java
+++ src/main/java/com/hcodez/cncapi/code/CodeController.java
@@ -1,15 +1,19 @@
 package com.hcodez.cncapi.code;
 
+import com.google.gson.Gson;
 import com.hcodez.codeengine.model.Code;
 import com.hcodez.codeengine.model.CodeType;
 import com.hcodez.codeengine.parser.CodeParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController("codeController")
 @RequestMapping("/code")
 public class CodeController {
 
@@ -23,23 +27,31 @@ public class CodeController {
 
     @RequestMapping(value = "/parse",
             method = RequestMethod.POST,
-            consumes = "text/plain",
-            produces = "application/json")
-    public List<String> parseInputString(@RequestBody String input) {
-        final List<Code> codeList = new CodeParser()
-                .addCodeTypes(CodeType.all())
-                .parseString(input);
-
-        final List<String> jsonStringList = new ArrayList<>();
-        for (Code code : codeList) {
-            jsonStringList.add(code.toJson());
-        }
-
-        return jsonStringList;
+            consumes = MediaType.TEXT_PLAIN_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CodeEntity>> parseInputString(/*@RequestBody String input*/) {
+//        final List<Code> codeList = new CodeParser()
+//                .addCodeTypes(CodeType.all())
+//                .parseString(input);
+//
+        final List<CodeEntity> codeEntityList = new ArrayList<>();
+//        for (Code code : codeList) {
+//            codeEntityList.add(CodeEntity.fromLibraryCode(code));
+//        }
+//
+//        return codeEntityList;
+        codeEntityList.add(CodeEntity
+                .builder()
+                .identifier("ab12")
+                .owner("dada3")
+                .passcode("dkl23mdasa")
+                .codeType(CodeType.PUBLIC_WITH_PASSCODE)
+                .build());
+        return new ResponseEntity<List<CodeEntity>>(codeEntityList, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET,
-            consumes = "application/json")
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CodeEntity> getByIdentifierAndOwner(@RequestParam String identifier, @RequestParam String owner) {
         return codeRepository.findByIdentifierAndOwner(identifier, owner);
     }
