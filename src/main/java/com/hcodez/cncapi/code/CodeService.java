09c534e948fcd3ecb54477d13f575f7f7779a871
cezarmathe <cezarmathe@gmail.com>
2019-08-11 16:45:53 +0300

    Refactor CodeService methods and add boilerplate for new services for CRUD operations

--- src/main/java/com/hcodez/cncapi/code/CodeService.java
+++ src/main/java/com/hcodez/cncapi/code/CodeService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,17 +17,38 @@ public class CodeService {
     @Autowired
     private CodeRepository codeRepository;
 
-    public CodeEntity createCode(@Nonnull CodeEntity codeEntity) {
-        codeEntity.setCreateTime(Instant.now());
-        codeEntity.setUpdateTime(Instant.now());
-        return codeRepository.save(codeEntity);
+
+    public List<CodeEntity> findCodes() { // FIXME: 2019-08-11 method must actually return all of a user's codes
+        return (List<CodeEntity>) codeRepository.findAll();
     }
 
     public List<CodeEntity> findCodeById(@Nonnull Integer id) {
-        Optional<CodeEntity> result = codeRepository.findById(id);
-        List<CodeEntity> list = new ArrayList<>();
+        final Optional<CodeEntity> result = codeRepository.findById(id);
+        final List<CodeEntity> list = new ArrayList<>();
 
         result.ifPresent(list::add);
         return list;
+    }
+
+    public CodeEntity createCode(@Nonnull CodeEntity codeEntity) {
+        codeEntity.setCreateTime(Instant.now());
+        codeEntity.setUpdateTime(Instant.now());
+
+        final CodeEntity newCodeEntity = codeRepository.save(codeEntity);
+        try {
+            newCodeEntity.setUrl(new URL("http://localhost:8080/code/" +
+                    newCodeEntity.getId()));
+        } catch (MalformedURLException e) {
+            return null;
+        }
+        return newCodeEntity;
+    }
+
+    public List<CodeEntity> updateCodeById(@Nonnull Integer id) {
+        return new ArrayList<>();
+    }
+
+    public boolean deleteCodeById(@Nonnull Integer id) {
+        return false;
     }
 }
